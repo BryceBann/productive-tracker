41c34598791c56eddc3b4cf7bd973a7295ac7f53
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-08-30 18:32:57 -0500

    last push before class, color doesnt display correctly,save btn doesnt show correctly, issues with the data being saved and page reloading

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,31 @@
-function grabLocalData(key){
+//pulls any previously saved data from local storage
+    function grabLocalData(key){
     let data = localStorage.getItem(key);
     if(data){
         $(`#text${key}`).text(value);
     }
 }
-
+//adds in the input divs for the work day hours
 $(document).ready(function(){
     $('#currentDay').text(moment().format("dddd, MMMMM Do, YYYY"));
     for(let i = 9; i < 18; i++) {
        
         var timeLine = $(`<div data-time=${i} id='${i}' class="row">`);
 
-        var firstCol = $('<div class="col-1"> <p class="hour"> ' + dayTimeAmPm(i) + '</p>');
+        var firstCol = $('<div class="col-sm-2"> <p class="hour"> ' + dayTimeAmPm(i) + '</p>');
 
-        var secondCol = $(`<div class="col-2"> <textarea id=text${i} class="inputArea" placeholder="Add whats going on"></textarea>`);
+        var secondCol = $(`<div class="col-sm-8"> <textarea id=text${i} class="description" placeholder="Add whats going on"></textarea>`);
 
-        var thirdCol = $(`<div class="col-3"><button class="saveBtn" id=${i}></button>`);
+        var thirdCol = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}></button>`);
 
         timeLine.append(firstCol);
         timeLine.append(secondCol);
         timeLine.append(thirdCol);
         $(".container").append(timeLine);
 
-        //localstorage
+        grabLocalData(i);
     }
-
+//creates the day and night cycle
     function dayTimeAmPm(hours){
         var ampm = hours >= 12 ? 'PM' : 'AM';
        hours = hours % 12;
@@ -32,7 +33,7 @@ $(document).ready(function(){
        return hours + ampm;
      }
    dayTimeAmPm();
-
+//changes the color when the time his in future current or past
    function colorChange() {
     var nowTime = new Date().getHours();
     for(var i = 9; i < 18; i++) {
@@ -40,11 +41,20 @@ $(document).ready(function(){
             $(`#text${i}`).addClass("present");
         }else if(nowTime < $(`#${i}`).data("time")) {
             $(`#text${i}`).addClass("future")
+        }else{
+            $(`#text${i}`).addClass("past")
         }
       }
    }
 
    setInterval(function() {
     colorChange();
-   }, 1000)
+   }, 1000);
+//makes the save btn event save data to the local storage 
+   var saveBtn = $('.saveBtn');
+   saveBtn.on('click', function(){
+    let btnClicked = $(this).attr('id');
+    let savedText = $(this).parent().siblings().children('.description').val();
+    localStorage.setItem(btnClicked, savedText);
+   });
 });
