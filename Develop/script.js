29da83e393a3218fb6810d4649d7df10c61dd1f1
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-08-30 14:50:23 -0500

    didnt have js linked, have rows and col showing needs more work

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,21 @@
+function grabLocalData(key){
+    let data = localStorage.getItem(key);
+    if(data){
+        $(`#text${key}`).text(value);
+    }
+}
+
 $(document).ready(function(){
     $('#currentDay').text(moment().format("dddd, MMMMM Do, YYYY"));
     for(let i = 9; i < 18; i++) {
        
-        var timeLine = $('<div data-time=${i} id=${i} class="row">');
+        var timeLine = $(`<div data-time=${i} id='${i}' class="row">`);
 
         var firstCol = $('<div class="col-1"> <p class="hour"> ' + dayTimeAmPm(i) + '</p>');
 
-        var secondCol = $('<div class="col-2"> <textarea id=text${i} class="inputArea" placeholder="Add whats going on"></textarea>');
+        var secondCol = $(`<div class="col-2"> <textarea id=text${i} class="inputArea" placeholder="Add whats going on"></textarea>`);
 
-        var thirdCol = $('<div class="col-3"><button class="saveBtn" id=${i}></button>');
+        var thirdCol = $(`<div class="col-3"><button class="saveBtn" id=${i}></button>`);
 
         timeLine.append(firstCol);
         timeLine.append(secondCol);
@@ -23,7 +30,21 @@ $(document).ready(function(){
        hours = hours % 12;
        hours = hours ? hours : 12;
        return hours + ampm;
-   }
+     }
    dayTimeAmPm();
-   
+
+   function colorChange() {
+    var nowTime = new Date().getHours();
+    for(var i = 9; i < 18; i++) {
+        if($(`#${i}`).data("time") == nowTime){
+            $(`#text${i}`).addClass("present");
+        }else if(nowTime < $(`#${i}`).data("time")) {
+            $(`#text${i}`).addClass("future")
+        }
+      }
+   }
+
+   setInterval(function() {
+    colorChange();
+   }, 1000)
 });
